a5d93e7db5696a62da61312d6fe821f6d12872c4
kevin <kevbriv@hotmail.fr>
2013-10-06 13:25:40 +0200

    Année du datepicker configurée

--- HelloGap/assets/www/js/tools.js
+++ HelloGap/assets/www/js/tools.js
@@ -3,6 +3,11 @@
  * Permet d'ouvrir l'interface native des dates et heures
  * 
  */
+var myDate = new Date();
+
+var dateDebut = myDate.getFullYear()-2;
+var dateFin = myDate.getFullYear()+50;
+
 function applyDatePicker() {
 	$('.date').mobiscroll().date({
         theme: 'android-ics light',
@@ -10,10 +15,11 @@ function applyDatePicker() {
         display: 'modal',
         mode: 'scroller',
         animate: 'pop',
-        invalid: [ 'w0', 'w6', '5/1', '12/24', '12/25' ]
+        startYear: dateDebut,
+        endYear: dateFin
     });
 	
-	// si clique sur le bouton "terminé" du datePicker, on quitte le focus du champs en cours
+	// si clique sur le bouton "terminée" du datePicker, on quitte le focus du champs en cours
 	$('.date').focus(function() {
 		$("span[class='dwbw dwb-s']").click(function() {
 			inputEnCours.blur();
